07f312c3678a2dc7d16ac273dc579c6071ae9a64
Dmitri Lipski <dlipski@klika-tech.com>
2019-08-07 18:31:56 +0300

    Implement modal window

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,91 @@
+module Main exposing (Msg(..), main, update, view)
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+
+-- MAIN
+
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
-type Msg = Increment | Decrement
 
+
+-- MODEL
+
+
+type alias Form =
+    { name : String
+
+    --    , age : Int
+    --    , password : String
+    }
+
+
+type alias Model =
+    { isModalOpen : Bool
+    , formData : Form
+    }
+
+
+init : Model
+init =
+    Model False { name = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Open
+    | Close
+
+
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Open ->
+            { model | isModalOpen = True }
 
-    Decrement ->
-      model - 1
+        Close ->
+            { model | isModalOpen = False }
 
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Open ] [ text "Open" ]
+        , viewModal model Close
+        ]
+
+
+getDisplayValue : Bool -> String
+getDisplayValue isVisible =
+    if isVisible == True then
+        "block"
+
+    else
+        "none"
+
+
+viewModal : Model -> msg -> Html msg
+viewModal model toMsg =
+    let
+        prop =
+            getDisplayValue model.isModalOpen
+    in
+    div [ style "display" prop, class "modal" ]
+        [ div [ class "modal-content" ]
+            [ span [ class "close", onClick toMsg ] [ text "&times;" ]
+            , p [] [ text "Some text in the Modal" ]
+            ]
+        ]
